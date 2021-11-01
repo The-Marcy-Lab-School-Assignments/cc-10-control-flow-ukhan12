9491b378f58f409cc91e68b1b65fb97bff939f36
uzma <ubkhan3@gmail.com>
2021-11-01 14:13:53 +0000

    complete

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 //1
-function greatestOfTwo(){
+function greatestOfTwo(num1, num2){
+  if(num1 > num2){
+      return num1;
+  }else 
+      return num2;
   
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(num1, num2, num3){
+  if(num1 > num2 && num1 > num3){
+      return num1;
+  }else if(num2 > num1 && num2 > num3){
+      return num2;
+  }else
+    return num3;
 }
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(num1, num2, num3, num4){
+  if(num1 > num2 && num1 > num3 && num1 > num4){
+      return num1;
+  }else if(num2 > num1 && num2 > num3 && num2 > num4){
+      return num2;
+  }else if(num3 > num1 && num3 > num2 && num3 > num4){
+    return num3;
+  }else
+    return num4;
 }
 
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(num1, num2, num3, num4){
+   if(num1 < num2 && num1 < num3 && num1 < num4){
+      return num1;
+  }else if(num2 < num1 && num2 < num3 && num2 < num4){
+      return num2;
+  }else if(num3 < num1 && num3 < num2 && num3 < num4){
+    return num3;
+  }else
+    return num4;
 }
+
+console.log(leastOfFour(7, 2, 3, 1))
